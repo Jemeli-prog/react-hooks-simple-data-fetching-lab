86b7ef15f3af3970d894ab0b96354e20674e6547
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-04-19 07:04:03 +0300

    fetch assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+function App(){
+    const [image, setImage] = useState("");
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+         .then(res => res.json())
+         .then(data => setImage(data.message))
+
+    },[])
+
+   
+    return(
+        <div>
+            
+ {image === "" ? ( <p>Loading ...</p>) : (<img src = {image} alt = "A Random Dog"/>)}
+ 
+
+        
+        
+        </div>
+    )
+
+}
+export default App;
